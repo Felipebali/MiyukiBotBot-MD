2871d094e1a2797fb48b0c9ece608d5cbd6426a6
Felipebali <felipebaliski@gmail.com>
2026-03-04 13:04:39 -0300

    Update grupo-chat.js

--- plugins/grupo-chat.js
+++ plugins/grupo-chat.js
@@ -1,13 +1,19 @@
 let handler = async (m, { conn, command }) => {
 
-    // Verificar que el chat exista en la base
     if (!(m.chat in global.db.data.chats)) {
         return conn.reply(m.chat, '🔥 *¡Este chat no está registrado!*', m)
     }
 
     let chat = global.db.data.chats[m.chat]
 
-    // 🔴 APAGAR BOT (.bc)
+    command = command.toLowerCase()
+
+    // 🟢 SI ESTÁ APAGADO, SOLO PERMITIR .ba
+    if (chat.isBanned && command !== 'ba') {
+        return conn.reply(m.chat, '🔒 *FelixCat-Bot está apagado en este grupo.*\nUsa *.ba* para activarlo.', m)
+    }
+
+    // 🔴 APAGAR BOT
     if (command === 'bc') {
 
         if (chat.isBanned) {
@@ -18,7 +24,7 @@ let handler = async (m, { conn, command }) => {
         return conn.reply(m.chat, '🔒 *FelixCat-Bot fue apagado en este grupo.*', m)
     }
 
-    // 🟢 ACTIVAR BOT (.ba)
+    // 🟢 ACTIVAR BOT
     if (command === 'ba') {
 
         if (!chat.isBanned) {
@@ -32,7 +38,7 @@ let handler = async (m, { conn, command }) => {
 
 handler.help = ['bc', 'ba']
 handler.tags = ['grupo']
-handler.command = ['bc', 'ba']
+handler.command = /^(bc|ba)$/i
 handler.owner = true
 handler.botAdmin = true
 handler.group = true
